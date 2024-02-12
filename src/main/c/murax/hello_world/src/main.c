74a58c02f923bef19c55ea90f4a7a69063736fb9
Ruslan Zalata <rz@fabmicro.ru>
2024-02-12 20:28:06 +0000

    Add C code to check performance of dalay()

--- src/main/c/murax/hello_world/src/main.c
+++ src/main/c/murax/hello_world/src/main.c
@@ -19,8 +19,25 @@ void println(const char*str){
 
 void delay(uint32_t loops){
 	for(int i=0;i<loops;i++){
-		int tmp = GPIO_A->OUTPUT;
+		//int tmp = GPIO_A->OUTPUT;
+		asm volatile ("slli t1,t1,0x10");
 	}
+}
+
+void printhex(unsigned int number){
+	unsigned int mask = 0xf0000000;
+	static char hex_digits[11] = {0,0,0,0,0,0,0,0,'\r','\n','\0'};
+
+	for(int i = 0; i < 8; i++) {
+		int digit = (number & mask) >> 4*(7-i);
+		if(digit < 0x0a)
+			hex_digits[i] = digit + 0x30;
+		else
+			hex_digits[i] = (digit - 0x0a) + 'A';
+		mask = mask >> 4;
+	}
+
+	print(hex_digits);
 }
 
 void main() {
@@ -40,16 +57,23 @@ void main() {
 	GPIO_A->OUTPUT = 0x00000001;
 	const int nleds = 4;
 	const int nloops = 1000000;
+	TIMER_PRESCALER->LIMIT = 0xFFFF; // Set max possible clock divider
 	while(1){
+		unsigned int shift_time;
 		println("Hello world, this is VexRiscv!");
 		for(unsigned int i=0;i<nleds-1;i++){
     			GPIO_A->OUTPUT = 1<<i;
+			TIMER_A->CLEARS_TICKS = 0x00020002;
+			TIMER_A->LIMIT = 0xFFFF;
     			delay(nloops);
+			shift_time = TIMER_A->VALUE; 
 		}
 		for(unsigned int i=0;i<nleds-1;i++){
 			GPIO_A->OUTPUT = (1<<(nleds-1))>>i;
 			delay(nloops);
 		}
+
+		printhex(shift_time);
 	}
 }
 
