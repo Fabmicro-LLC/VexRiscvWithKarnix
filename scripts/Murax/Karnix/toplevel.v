6ae955b1129b995c86b6dbb7e488d88b187fcf1d
Ruslan Zalata <rz@fabmicro.ru>
2024-03-03 00:52:48 +0000

    Add PHY signals to toplevel.v and lower clk to 75.0 MHz

--- scripts/Murax/Karnix/toplevel.v
+++ scripts/Murax/Karnix/toplevel.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
 //`include "pll25to81.v"
-`include "pll25to80.v"
-//`include "pll25to75.v"
+//`include "pll25to80.v"
+`include "pll25to75.v"
 //`include "pll25to60.v"
 //`include "pll25to58.v"
 //`include "pll25to50.v"
@@ -23,7 +23,16 @@ module toplevel(
     output  io_sram_bhe, // #UB
     output  io_sram_ble, // #LB
     output  [17:0] io_sram_addr, // A0-A17
-    inout   [15:0] io_sram_dat   // D0-D15
+    inout   [15:0] io_sram_dat,  // D0-D15
+    input   [3:0] io_mii_RX_D, // MII RX data lines 
+    input   io_mii_RX_DV, // MII RX data valid 
+    input   io_mii_RX_CLK, // MII RX clock
+    input   io_mii_RX_ER, // MII RX error flag
+    input   io_mii_RX_CRS, // MII RX carrier sense flag
+    input   io_mii_RX_COL, // MII RX collision detection flag
+    output  [3:0] io_mii_TX_D, // MII TX data lines
+    output  io_mii_TX_EN, // MII TX data enable 
+    input  io_mii_TX_CLK, // MII TX clock
   );
 
   assign io_led[0] = io_gpioA_write[0];
@@ -44,8 +53,8 @@ module toplevel(
 
   //assign clk = clk25;
   //pll i_pll(.in_clk25(io_clk25), .out_clk81(clk));
-  pll i_pll(.in_clk25(io_clk25), .out_clk80(clk));
-  //pll i_pll(.in_clk25(io_clk25), .out_clk75(clk));
+  //pll i_pll(.in_clk25(io_clk25), .out_clk80(clk));
+  pll i_pll(.in_clk25(io_clk25), .out_clk75(clk));
   //pll i_pll(.in_clk25(io_clk25), .out_clk60(clk));
   //pll i_pll(.in_clk25(io_clk25), .out_clk58(clk));
   //pll i_pll(.in_clk25(io_clk25), .out_clk50(clk));
@@ -68,8 +77,16 @@ module toplevel(
     .io_sram_bhe(io_sram_bhe),
     .io_sram_ble(io_sram_ble),
     .io_sram_addr(io_sram_addr),
-    .io_sram_dat(io_sram_dat)
+    .io_sram_dat(io_sram_dat),
+    .io_mii_RX_D(io_mii_RX_D),
+    .io_mii_RX_CLK(io_mii_RX_CLK),
+    .io_mii_RX_DV(io_mii_RX_DV),
+    .io_mii_RX_ER(io_mii_RX_ER),
+    .io_mii_RX_CRS(io_mii_RX_CRS),
+    .io_mii_RX_COL(io_mii_RX_COL),
+    .io_mii_TX_D(io_mii_TX_D),
+    .io_mii_TX_CLK(io_mii_TX_CLK),
+    .io_mii_TX_EN(io_mii_TX_EN),
   );
-
 
 endmodule
